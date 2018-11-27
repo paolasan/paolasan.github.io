2d6b99b34b326eb4c812eb692739f77fd1b6dab3
Cordoba25 <mateocordoba25@gmail.com>
2018-11-26 21:36:30 -0300

    display modal image function change, use index

--- image-galery.js
+++ image-galery.js
@@ -2,6 +2,8 @@ let m_imageArray = document.images;
 // Get the modal
 var modal = document.getElementById('myModal');
 
+let selectedImageIndex = 0;
+
 for (f_index = 0; f_index < m_imageArray.length; f_index++) {
     let f_image = m_imageArray[f_index];
 
@@ -12,24 +14,24 @@ for (f_index = 0; f_index < m_imageArray.length; f_index++) {
     else {
         f_image.addEventListener('load', showImage(f_image));
     }
+
+    setModalLogic(f_index);
 }
 
 function showImage(p_image) {
     p_image.style.visibility = 'visible';
     p_image.classList.add("on-image-loaded");
-
-    setModalLogic(p_image);
 }
 
 // Modal stuff.
 
-function setModalLogic(p_image) {
-    var modalImg = document.getElementById("img01");
-    var captionText = document.getElementById("caption");
-    p_image.onclick = function(){
-        modal.style.display = "block";
-        modalImg.src = this.src;
-        captionText.innerHTML = this.alt;
+function setModalLogic(p_index) {
+
+    let m_image = m_imageArray[p_index];
+    selectedImageIndex = p_index;
+
+    m_image.onclick = function(){
+        setModalImage(p_index);
     }
 
     // Get the <span> element that closes the modal
@@ -38,5 +40,40 @@ function setModalLogic(p_image) {
     // When the user clicks on <span> (x), close the modal
     span.onclick = function() { 
     modal.style.display = "none";
+    }
+}
+
+function setModalImage(p_index) {
+    var m_image = m_imageArray[p_index];
+    modal.style.display = "block";
+    let m_modal = document.getElementById("img01");
+    m_modal.src = m_image.src;
+}
+
+document.addEventListener('keydown', function (p_event) {
+    var m_keyCode = p_event.keyCode;
+   
+    let m_next = 0;
+
+    if(m_keyCode == 37) {
+        m_next = 1;
+    }
+
+    else if (m_keyCode == 39) {
+        m_next = -1;
+    }
+
+    nextImage(m_next);
+});
+
+function nextImage(p_next) {
+    selectedImageIndex += p_next;
+
+    if(selectedImageIndex > m_imageArray.length) {
+        selectedImageIndex = 0;
+    }
+
+    if(selectedImageIndex < 0) {
+        selectedImageIndex = m_imageArray.length -1;
     }
 }
