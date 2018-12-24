2618bfea9dc9008f47f9f7b32c1712a91ce4ebfc
Leo Mac <leagamedev@gmail.com>
2018-12-24 11:34:10 -0300

    post request test

--- contact.js
+++ contact.js
@@ -14,3 +14,42 @@ for(f_index = 0; f_index < inputArray.length; f_index++) {
         m_parent.children[1].classList.add('hide-line');
     });
 }
+
+submintButton = document.getElementById('submit-button');
+
+nameField = document.getElementById('contact-name');
+subjectField = document.getElementById('contact-subject');
+emailField = document.getElementById('contact-email');
+contentField = document.getElementById('contact-body');
+
+submintButton.addEventListener('click', function() {
+    
+    // TODO: don't send function with empty field ( like email, body).
+
+    // Create Message.
+    let m_content = contentField.value + "\n" + nameField.value;
+
+    let m_json = JSON.stringify({
+        email : emailField.value,
+        subject : subjectField.value,
+        content : m_content
+    });
+
+    console.log(m_json);
+
+    postRequest(m_json);
+});
+
+function postRequest(p_json) {
+    const m_url = 'https://us-central1-portfolio-1717.cloudfunctions.net/contact';
+
+    const m_parameters = {
+        headers: {
+            'content-type':'application/json'
+        },
+        body: p_json,
+        method: 'POST'
+    }
+
+    fetch(m_url, m_parameters);
+}
